3abe2591acd9422d932ef3db1c0c7e4c6caa406d
Raju Komati <komatiraju032@gamil.com>
2025-03-31 11:23:12 +0530

    added chapter-10

---
+++ src/main/java/chapter10/DynamicOrderDeadlock.java
@@ -0,0 +1,82 @@
+package chapter10;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * DynamicOrderDeadlock
+ * <p/>
+ * Dynamic lock-ordering deadlock
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class DynamicOrderDeadlock {
+    // Warning: deadlock-prone!
+    public static void transferMoney(Account fromAccount,
+                                     Account toAccount,
+                                     DollarAmount amount)
+            throws InsufficientFundsException {
+        synchronized (fromAccount) {
+            synchronized (toAccount) {
+                if (fromAccount.getBalance().compareTo(amount) < 0)
+                    throw new InsufficientFundsException();
+                else {
+                    fromAccount.debit(amount);
+                    toAccount.credit(amount);
+                }
+            }
+        }
+    }
+
+    static class DollarAmount implements Comparable<DollarAmount> {
+        private  int balance;
+
+        public DollarAmount(int amount) {
+            balance = amount;
+        }
+
+        public DollarAmount add(DollarAmount d) {
+            balance += d.balance;
+            return this;
+        }
+
+        public DollarAmount subtract(DollarAmount d) {
+            balance -= d.balance;
+            return this;
+        }
+
+        public int compareTo(DollarAmount dollarAmount) {
+            return Integer.compare(balance, dollarAmount.balance);
+        }
+    }
+
+    static class Account {
+        private DollarAmount balance;
+        private final int acctNo;
+        private static final AtomicInteger sequence = new AtomicInteger();
+
+        public Account() {
+            acctNo = sequence.incrementAndGet();
+            balance = new DollarAmount(100);
+        }
+
+        void debit(DollarAmount d) {
+            balance = balance.subtract(d);
+        }
+
+        void credit(DollarAmount d) {
+            balance = balance.add(d);
+        }
+
+        DollarAmount getBalance() {
+            return balance;
+        }
+
+        int getAcctNo() {
+            return acctNo;
+        }
+    }
+
+    static class InsufficientFundsException extends Exception {
+    }
+}
